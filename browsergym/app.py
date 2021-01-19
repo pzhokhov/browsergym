c3d55c8497310328da2ed36c69b6c67bf96187a3
Peter Zhokhov <peterz@openai.com>
2021-01-19 13:20:50 -0800

    .

--- browsergym/app.py
+++ browsergym/app.py
@@ -33,5 +33,5 @@ def observe():
 
 if __name__ == '__main__':
     app.env = make_env()
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0", port=80)
     app.env.env.reset()
--- browsergym/router.py
+++ browsergym/router.py
@@ -9,31 +9,27 @@ statuses = {}
 
 
 def check_worker(url):
-    timestamp = requests.get(url + "/status", timeout=1.0)
-    last_status = statuses[url]
-    if timestamp != None:
-        statuses[url] = ("ok", int(timestamp))
+    response = requests.get(url + "/status", timeout=1.0)
+    if response.ok:
+        statuses[url] = ("ok", float(response.text))
     else:
         statuses[url] = ("fail", None)
 
-
 def check_workers_loop():
-    for wurl in statuses.keys():
-        check_worker(wurl)
-    time.sleep(1) 
-
+    while True:
+        for wurl in statuses.keys():
+            check_worker(wurl)
+        time.sleep(1) 
 
 def choose_worker():
-    ok_workers = [(u, s[1]) for  u, s in statuses.items() if s[0] == 'ok')
+    ok_workers = [(u, t) for  u, (s, t) in statuses.items() if s == 'ok']
     if len(ok_workers) < 1:
         raise ValueError("No valid workers!")
     ok_workers.sort(key=lambda x: x[1])
     return ok_workers[0][0]
 
-
 def register_worker(url):
     statuses[url] = ("ok", 0)
-    
 
 @app.route('/')
 def index():
@@ -41,5 +37,6 @@ def index():
     return flask.redirect(url)
 
 if __name__ == '__main__':
+    register_worker('http://192.168.8.92:5000')
     Thread(target=check_workers_loop).start()
     app.run()
