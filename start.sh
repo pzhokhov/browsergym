d4d1e1b80f46b01be12bcc87910396439031f5ac
Peter Zhokhov <peterz@openai.com>
2021-01-19 15:34:51 -0800

    .

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 # start upload of the recordings to azure
-# 
+
+
+
+function upload_recordings {
+    while true; do
+        echo "bbb sync $HOME/minerl_recordings az://oaiagidata/data/datasets/minerl_recorder/v1/"
+        sleep 10
+    done
+}
+
+upload_recordings&
